151e21c6fd5b2e4061e54c59f7d41653362a8415
plzfgme <m19959605070@163.com>
2023-01-11 16:49:51 +0800

    feat(fastio): add new scheme fastio

---
+++ fastio/README.md
@@ -0,0 +1,7 @@
+# FASTIO
+
+The scheme is presented by
+
+|Article|Author|
+|-|-|
+|[Forward Private Searchable Symmetric Encryption with Optimized I/O Efficiency](https://arxiv.org/pdf/1710.00183.pdf)|Xiangfu Song, Changyu Dong, Dandan Yuan, Qiuliang Xu and Minghao Zhao|
---
+++ fastio/doc.go
@@ -0,0 +1,8 @@
+// Package fastio provides FASTIO scheme presented by Xiangfu Song, Changyu Dong, Dandan Yuan, Qiuliang Xu and Minghao Zhao.
+//
+// Article: [Forward Private Searchable Symmetric Encryption with Optimized I/O Efficiency]
+//
+// Author: Xiangfu Song, Changyu Dong, Dandan Yuan, Qiuliang Xu and Minghao Zhao
+//
+// [Forward Private Searchable Symmetric Encryption with Optimized I/O Efficiency]: https://arxiv.org/pdf/1710.00183.pdf
+package fastio
